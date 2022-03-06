3ed5b085043bb80ee22980ef9ce3f440894a98b5
Javier Santiago Salazar <javiersantiago45011@gmail.com>
2022-03-06 10:35:49 -0500

    REFACTOR - Package name in introductor activity and clean packages

--- app/src/main/java/com/javierestudio/thesportsdb/ui/teamModule/IntroductorActivity.kt
+++ app/src/main/java/com/javierestudio/thesportsdb/ui/teamModule/IntroductorActivity.kt
@@ -1,16 +1,13 @@
-package com.javierestudio.thesportsdb
+package com.javierestudio.thesportsdb.ui.teamModule
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
 import com.javierestudio.thesportsdb.databinding.ActivityIntroductorBinding
-import com.javierestudio.thesportsdb.ui.teamModule.TeamActivity
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
